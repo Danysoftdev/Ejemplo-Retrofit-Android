fd8723a9b55cc6751751faf7a3cf44e5d3e6980e
Danysoftdev <danysoftdev@gmail.com>
2024-05-17 11:31:10 -0500

    creadas clases y layouts necesarios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation (libs.retrofit)
     implementation (libs.converter.gson)
     implementation (libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/co/edu/eam/ejemploretrofit/DogsResponse.kt
@@ -0,0 +1,4 @@
+package co.edu.eam.ejemploretrofit
+
+class DogsResponse {
+}
